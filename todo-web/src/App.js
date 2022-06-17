8b2775f5d2a0d377f6cc6f8ea69c36d9c80cd827
shuhei-T <nofuture742617000027@gmail.com>
2022-06-17 21:31:58 +0900

    React Hooksに学んで、実際に使ってみる

--- todo-web/src/App.js
+++ todo-web/src/App.js
@@ -1,7 +1,35 @@
 import Task from "./components/Task";
 import { Center, Box, CheckboxGroup, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const initialTasks = [
+    {
+      name: "買い物",
+      isDone: true,
+    },
+    {
+      name: "ランニング",
+      isDone: false,
+    },
+    {
+      name: "プログラミングの勉強",
+      isDone: false,
+    },
+  ];
+
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    setTasks(initialTasks);
+  }, []);
+
+  const toggleIsDone = (index) => {
+    const tasksCopy = [...tasks];
+    const isDone = tasksCopy[index].isDone;
+    tasksCopy[index].isDone = !isDone;
+    setTasks(tasksCopy);
+  };
+
   return (
     <Box mt="64px">
       <Center>
@@ -12,9 +40,17 @@ function App() {
             </Text>
           </Box>
           <CheckboxGroup>
-            <Task name="買い物" />
-            <Task name="ランニング" />
-            <Task name="プログラミングの勉強" />
+            {tasks.map((task, index) => {
+              return (
+                <Task
+                  key={index}
+                  index={index}
+                  name={task.name}
+                  isDone={task.isDone}
+                  toggleIsDone={toggleIsDone}
+                />
+              );
+            })}
           </CheckboxGroup>
         </Box>
       </Center>
